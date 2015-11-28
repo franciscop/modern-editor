f73993cd422a3b2aa4d9454a59b277be5a21a999
FranciscoP <franciscopresencia@hotmail.com>
2015-11-28 19:40:39 +0100

    Added a better method to wrap text in an element

--- javascript.js
+++ javascript.js
@@ -4,43 +4,53 @@ var editor = new Editor("article");
 // Register a new action called "bold"
 editor.add("bold", {
   menu: "<strong>B</strong>", shortcut: "ctrl+b",
-  action: function(e) {
-    this.command("bold");
+  action: function(editor) {
+    editor.command("bold");
   }
 });
 
 // Register a new action called "italic"
 editor.add("italic", {
   menu: "<i>i</i>", shortcut: "ctrl+i",
-  action: function() {
-    this.command("italic");
+  action: function(editor) {
+    editor.command("italic");
   }
 });
 
 // Register a new action called "link" with 
 editor.add("link", {
   menu: "<i class='icon-link'></i>", shortcut: "ctrl+k",
-  action: function() {
-    var link = this.selection.element.getAttribute("href") || "";
+  action: function(editor) {
+    var link = editor.selection.element.getAttribute("href") || "";
     var address = prompt("Link address", link);
     if (address) {
-      this.command('createLink', address);
+      editor.tag('a', { href: address });
+    } else {
+      editor.tag('a');
     }
   }
 });
 
-// Register a new action called "italic"
+// // Register a new action called "code"
+// editor.add("code", {
+//   menu: "<i class='icon-terminal'></i>", shortcut: "ctrl+`",
+//   action: function(editor) {
+//     editor.command("insertHtml", "<code>" + this.selection.text + "</code>");
+//   }
+// });
+
+
 editor.add("code", {
   menu: "<i class='icon-terminal'></i>", shortcut: "ctrl+`",
-  action: function() {
-    this.command("insertHtml", "<code>" + this.selection.text + "</code>");
+  action: function(editor) {
+    editor.tag("code", { class: "bla" });
   }
 });
 
 // Register a new action called "italic"
 editor.add('info', {
   menu: "<i class='icon-help'></i>",
-  action: function(){
+  action: function(editor){
     window.open("https://github.com/franciscop/modern-editor", "_blank");
   }
 });
